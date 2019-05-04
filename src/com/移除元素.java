b6b63d19e9b2c1dfd34ad6215ee62888ab34b8bd
“githe <562004191@qq.com>
2019-05-04 22:06:17 +0800

    原地算法

---
+++ src/com/移除元素.java
@@ -0,0 +1,26 @@
+package com;
+
+public class 移除元素 {
+    public static void main(String[] args) {
+        int[] n = new int[1];
+        n[0] = 3;
+//        n[1] = 2;
+//        n[2] = 2;
+//        n[3] = 3;
+        removeElement(n,3);
+    }
+    public static int removeElement(int[] nums, int val) {
+        int idex = 0;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == val){
+                continue;
+            }else {
+                nums[idex] = nums[i];
+                idex++;
+            }
+        }
+//        System.out.println(idex);
+        return idex;
+    }
+}
