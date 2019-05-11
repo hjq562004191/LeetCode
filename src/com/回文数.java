ed1d08cca2c1245f1c16804cb9c7dfc2b7c96765
“githe <562004191@qq.com>
2019-05-11 09:17:44 +0800

    无

---
+++ src/com/回文数.java
@@ -0,0 +1,53 @@
+package com;
+
+/**
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ *
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ *
+ * 你能不将整数转为字符串来解决这个问题吗？
+ */
+public class 回文数 {
+    public static void main(String[] args) {
+        boolean f = isPalindrome(121);
+        System.out.println(f);
+    }
+    public static boolean isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+        if (x == 0){
+            return true;
+        }
+
+        int i = 0;
+        int[] a = new int[32];
+        while (x !=  0){
+            a[i++] = x%10;
+            x /= 10;
+        }
+
+        for (int j = 0; j < i / 2; j++) {
+            if (a[j] == a[i-j-1]){
+                continue;
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
