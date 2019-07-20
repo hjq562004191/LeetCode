ac22510a0565e66a0188a3bd33fde0859e7fdb4b
hjq562004191 <562004191@qq.com>
2019-07-20 17:11:44 +0800

    后节点的值覆盖前节点的值

---
+++ src/com/删除链表中的节点.java
@@ -0,0 +1,65 @@
+package com;
+
+import java.util.List;
+
+/**
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ *
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+ *
+ *
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: head = [4,5,1,9], node = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ *
+ * 输入: head = [4,5,1,9], node = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *  
+ *
+ * 说明:
+ *
+ * 链表至少包含两个节点。
+ * 链表中所有节点的值都是唯一的。
+ * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ * 不要从你的函数中返回任何结果。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 删除链表中的节点 {
+    static ListNode head = new ListNode(4);
+    public static void main(String[] args) {
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+        deleteNode(head.next);
+    }
+    public static void deleteNode(ListNode node) {
+        while (node.next != null){
+            node.val = node.next.val;
+            if (node.next.next == null){
+                node.next = null;
+                break;
+            }
+            node = node.next;
+        }
+
+        while (head !=null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+}
