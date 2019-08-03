dd9ac78e67d9e4bc9e0d7572da9d4ba7f1f694db
hjq562004191 <562004191@qq.com>
2019-08-03 11:16:06 +0800

    每次寻找当前步下的最远

---
+++ src/com/跳跃游戏II.java
@@ -0,0 +1,43 @@
+package com;
+/**
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ *
+ * 示例:
+ *
+ * 输入: [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ *      从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ * 说明:
+ *
+ * 假设你总是可以到达数组的最后一个位置。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jump-game-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 跳跃游戏II {
+    public static void main(String[] args) {
+        System.out.println(jump(new int[]{2,1,3,1,1,2}));
+    }
+    public static int jump(int[] nums) {
+        if(nums.length == 1) return 0;
+        int reach = 0;
+        int nextreach = nums[0];
+        int step = 0;
+        for(int i = 0;i<nums.length;i++){
+            nextreach = Math.max(i+nums[i],nextreach);
+            if(nextreach >= nums.length-1) return (step+1);
+            if(i == reach){
+                step++;
+                reach = nextreach;
+            }
+        }
+        return step;
+    }
+    
+}
