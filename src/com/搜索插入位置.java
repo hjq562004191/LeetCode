78474d8377d10c6da0f6c7d4c1ca00fe472d07a6
hjq562004191 <562004191@qq.com>
2019-09-08 21:27:02 +0800

    9.8

--- src/com/搜索插入位置.java
+++ src/com/搜索插入位置.java
@@ -2,36 +2,37 @@ package com;
 
 /**
  * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
- *
+ * <p>
  * 你可以假设数组中无重复元素。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: [1,3,5,6], 5
  * 输出: 2
  * 示例 2:
- *
+ * <p>
  * 输入: [1,3,5,6], 2
  * 输出: 1
  * 示例 3:
- *
+ * <p>
  * 输入: [1,3,5,6], 7
  * 输出: 4
  * 示例 4:
- *
+ * <p>
  * 输入: [1,3,5,6], 0
  * 输出: 0
  */
 public class 搜索插入位置 {
     public static void main(String[] args) {
-        int[] m = {1,3,5,6};
-        int t = searchInsert(m,7);
+        int[] m = {1, 3, 5, 6};
+        int t = searchInsert(m, 7);
         System.out.println(t);
     }
+
     public static int searchInsert(int[] nums, int target) {
         int i;
-        for (i = 0; i <nums.length; i++) {
-            if (target <= nums[i]){
+        for (i = 0; i < nums.length; i++) {
+            if (target <= nums[i]) {
                 break;
             }
         }
