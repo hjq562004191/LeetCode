486fe06df0cf1ad38a79cf3320ac28098ac4ccef
hjq562004191 <562004191@qq.com>
2019-11-21 21:29:41 +0800

    选择

--- src/Sort/选择.java
+++ src/Sort/选择.java
@@ -3,13 +3,18 @@ package Sort;
 public class 选择 {
     public static void main(String[] args) {
         int[] nums = {9, 5, 2, 7, 1};
+        int index = 0;
         for (int i = 0; i < nums.length - 1; i++) {
+            index = i;
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] > nums[j]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
+                if (nums[index] > nums[j]) {
+                    index = j;
                 }
+            }
+            if (index != i) {
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
             }
             for (int n : nums
             ) {
